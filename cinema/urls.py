797a9b2c311e3290fea0d37e12bb3d70b5615cf6
Glego <glego200323@gmail.com>
2025-09-10 11:33:17 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -22,6 +22,9 @@ router.register("orders", OrderViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
 app_name = "cinema"
